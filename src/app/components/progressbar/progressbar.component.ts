a66cce55f21d5145e6a3104dcb2f6aafd85e3800
Christoph Kissmann <kissmannchristoph@gmail.com>
2022-02-03 13:43:16 +0100

    idk

--- src/app/components/progressbar/progressbar.component.ts
+++ src/app/components/progressbar/progressbar.component.ts
@@ -1,10 +1,1 @@
-import { Component, OnInit } from "angular-ts-decorators";
 
-@Component({
-  selector: "app-comp-progressbar",
-  template: require("./progressbar.html"),
-  styles: [require("./style.css")]
-})
-export class ProgressbarComponent implements OnInit {
-  ngOnInit(): void {}
-}
